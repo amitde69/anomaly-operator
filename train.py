8c71185235eb9af125e7415a21f5a787fe5ff652
amitde69 <amitde7896@gmail.com>
2023-03-02 03:03:24 +0200

    added sql cpu metric

--- train.py
+++ train.py
@@ -36,6 +36,8 @@ def detect_cycle(config):
             m.fit(df)
             future = m.make_future_dataframe(periods=0) 
             forecast = m.predict(future)
+            future = m.make_future_dataframe(periods=0) 
+            forecast = m.predict(future)
             if local == 1:
                 fig = None
                 ax = None
